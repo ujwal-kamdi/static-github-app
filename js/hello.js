1b2eaacd25466e0316636c45d8161cfec921af05
Ujwal Kamdi <ujwalkamdi05@gmail.com>
2025-01-15 11:37:14 +0530

    Commit made from VS Code Azure Static Web Apps

--- js/hello.js
+++ js/hello.js
@@ -7,7 +7,7 @@ function HelloWorld(id) {
   greeting = document.createElement('h3');
   greeting.setAttribute('id', 'hello');
   greeting.style.visibility = "hidden";
-  greeting.innerHTML = "Hello World!";
+  greeting.innerHTML = "Hello World! from Ujwal";
 
   element.appendChild(greeting);
 
